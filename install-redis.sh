4afe054672dadb65072721c84cb842a948c7eb95
ethan <brilon@outlook.com>
2021-10-24 13:01:32 +0800

    update

--- install-redis.sh
+++ install-redis.sh
@@ -7,27 +7,9 @@
 set -e
 set -x
 
-redis_ver=6.2.6
-download_dir=/down/redis
-
-if [ -f "/usr/local/bin/redis-server" ]; then
-  read -p "already installed. confirm to re-install[y/N]: " confirm
-  if [[ -z $confirm || "$confirm" == "N" || "$confirm" == "n" ]]; then
-    exit 0
-  fi
-fi
-
 apt update
-apt install build-essential pkg-config tcl -y
+apt install redis-server -y
 
-mkdir -p ${download_dir}
-cd ${download_dir}
-
-wget https://download.redis.io/releases/redis-${redis_ver}.tar.gz -O redis-${redis_ver}.tar.gz
-tar xzf redis-${redis_ver}.tar.gz
-cd redis-${redis_ver}
-
-make -j $(nproc)
-# disable test. cos no enough memory
-# make test
-make install
+sed -i 's/supervised no/supervised systemd/' /etc/redis/redis.conf
+systemctl restart redis-server.service
+netstat -ntl
