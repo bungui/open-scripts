0278307393210c1e2bd0cebcdac89ebad328d3cb
ethan <brilon@outlook.com>
2022-01-01 20:30:12 +0800

    update

--- client.sh
+++ client.sh
@@ -7,243 +7,284 @@ arch=$(uname -m)
 virt=$(systemd-detect-virt)
 kernelVer=$(uname -r)
 home_dir=$(
-  cd ~
-  pwd
+	cd ~
+	pwd
 )
 
 green() {
-  echo -e "\033[32m\033[01m$1\033[0m"
+	echo -e "\033[32m\033[01m$1\033[0m"
 }
 
 red() {
-  echo -e "\033[31m\033[01m$1\033[0m"
+	echo -e "\033[31m\033[01m$1\033[0m"
 }
 
 yellow() {
-  echo -e "\033[33m\033[01m$1\033[0m"
+	echo -e "\033[33m\033[01m$1\033[0m"
 }
 
 if [[ -f /etc/redhat-release ]]; then
-  release="Centos"
+	release="Centos"
 elif cat /etc/issue | grep -q -E -i "debian"; then
-  release="Debian"
+	release="Debian"
 elif cat /etc/issue | grep -q -E -i "ubuntu"; then
-  release="Ubuntu"
+	release="Ubuntu"
 elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
-  release="Centos"
+	release="Centos"
 elif cat /proc/version | grep -q -E -i "debian"; then
-  release="Debian"
+	release="Debian"
 elif cat /proc/version | grep -q -E -i "ubuntu"; then
-  release="Ubuntu"
+	release="Ubuntu"
 elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
-  release="Centos"
+	release="Centos"
 else
-  red "不支持你当前系统，请使用Ubuntu、Debian、Centos的主流系统"
-  exit 1
+	red "不支持你当前系统，请使用Ubuntu、Debian、Centos的主流系统"
+	exit 1
 fi
 
 if ! type curl >/dev/null 2>&1; then
-  yellow "curl未安装，安装中"
-  if [ $release = "Centos" ]; then
-    yum -y update && yum install curl -y
-  else
-    apt-get update -y && apt-get install curl -y
-  fi
+	yellow "curl未安装，安装中"
+	if [ $release = "Centos" ]; then
+		yum -y update && yum install curl -y
+	else
+		apt-get update -y && apt-get install curl -y
+	fi
 else
-  green "curl已安装"
+	green "curl已安装"
 fi
 
 if ! type wget >/dev/null 2>&1; then
-  yellow "wget未安装，安装中"
-  if [ $release = "Centos" ]; then
-    yum -y update && yum install wget -y
-  else
-    apt-get update -y && apt-get install wget -y
-  fi
+	yellow "wget未安装，安装中"
+	if [ $release = "Centos" ]; then
+		yum -y update && yum install wget -y
+	else
+		apt-get update -y && apt-get install wget -y
+	fi
 else
-  green "wget已安装"
+	green "wget已安装"
 fi
 
 if ! type sudo >/dev/null 2>&1; then
-  yellow "sudo未安装，安装中"
-  if [ $release = "Centos" ]; then
-    yum -y update && yum install sudo -y
-  else
-    apt-get update -y && apt-get install sudo -y
-  fi
+	yellow "sudo未安装，安装中"
+	if [ $release = "Centos" ]; then
+		yum -y update && yum install sudo -y
+	else
+		apt-get update -y && apt-get install sudo -y
+	fi
 else
-  green "sudo已安装"
+	green "sudo已安装"
 fi
 
 function update_script() {
-  timestamp=$(date +%s)
-  wget "https://raw.githubusercontent.com/bungui/open-scripts/dev/client.sh?t=${timestamp}" -O "${home_dir}/client.sh" &&
-    chmod -R 777 "${home_dir}/client.sh"
-  clear
-  bash "${home_dir}/client.sh"
+	timestamp=$(date +%s)
+	wget "https://raw.githubusercontent.com/bungui/open-scripts/dev/client.sh?t=${timestamp}" -O "${home_dir}/client.sh" &&
+		chmod -R 777 "${home_dir}/client.sh"
+	clear
+	bash "${home_dir}/client.sh"
 }
 
 function install_github_cli() {
-  # 参考： https://github.com/cli/cli/blob/trunk/docs/install_linux.md
-  if ! type gh >/dev/null 2>&1; then
-    yellow "gh未安装，安装中"
-    if [ $release = "Centos" ]; then
-      sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-      sudo dnf install gh
-    else
-      curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
-      sudo apt update
-      sudo apt install gh
-    fi
-  else
-    green "gh已安装，更新中"
-    if [ $release = "Centos" ]; then
-      sudo dnf update gh
-    else
-      sudo apt update
-      sudo apt install gh
-    fi
-  fi
+	# 参考： https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+	if ! type gh >/dev/null 2>&1; then
+		yellow "gh未安装，安装中"
+		if [ $release = "Centos" ]; then
+			sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+			sudo dnf install gh
+		else
+			curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+			echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null
+			sudo apt update
+			sudo apt install gh
+		fi
+	else
+		green "gh已安装，更新中"
+		if [ $release = "Centos" ]; then
+			sudo dnf update gh
+		else
+			sudo apt update
+			sudo apt install gh
+		fi
+	fi
 }
 
 function gh_login() {
-  gh auth status
-  result=$?
-  if [ $result -eq 1 ]; then
-    echo "未登陆github"
-    gh auth login
-  else
-    echo "已经登陆"
-  fi
+	gh auth status
+	result=$?
+	if [ $result -eq 1 ]; then
+		echo "未登陆github"
+		gh auth login
+	else
+		echo "已经登陆"
+	fi
 }
 
 function check_virtualenv() {
 
-  if [ ! -z "$VIRTUAL_ENV" ]; then
-    red "已经在virtualenv中"
-    return 1
-  fi
+	if [ ! -z "$VIRTUAL_ENV" ]; then
+		red "已经在virtualenv中"
+		return 1
+	fi
 
-  dpkg -s python3-virtualenv
-  result=$?
-  if [ $result -eq 1 ]; then
-    red "未安装python3-virtualenv"
-    sudo apt update
-    sudo apt install python3 python3-pip python3-virtualenv
-  fi
-  if [ ! -d .git ]; then
-    red "不是仓库根目录"
-    exit 1
-  fi
-  if [ ! -d venv ]; then
-    virtualenv venv
-  fi
-  echo "进入python虚拟环境"
-  source venv/bin/activate
-  yellow "安装依赖包"
-  pip install --upgrade pip
-  pip install -r requirements.txt
+	dpkg -s python3-virtualenv
+	result=$?
+	if [ $result -eq 1 ]; then
+		red "未安装python3-virtualenv"
+		sudo apt update
+		sudo apt install python3 python3-pip python3-virtualenv
+	fi
+	if [ ! -d .git ]; then
+		red "不是仓库根目录"
+		exit 1
+	fi
+	if [ ! -d venv ]; then
+		virtualenv venv
+	fi
+	echo "进入python虚拟环境"
+	source venv/bin/activate
+	yellow "安装依赖包"
+	pip install --upgrade pip
+	pip install -r requirements.txt
 }
 
 function clone_client_repo() {
-  if [ -d /repo/py-aiohttp-client ]; then
-    echo "仓库已经克隆"
-    cd /repo/py-aiohttp-client
-    git pull
-    check_virtualenv
-  else
-    mkdir -p /repo
-    cd /repo
-    gh repo clone brilon/py-aiohttp-client
-    cd py-aiohttp-client
-    check_virtualenv
-  fi
+	if [ -d /repo/py-aiohttp-client ]; then
+		echo "仓库已经克隆"
+		cd /repo/py-aiohttp-client
+		git pull
+		check_virtualenv
+	else
+		mkdir -p /repo
+		cd /repo
+		gh repo clone brilon/py-aiohttp-client
+		cd py-aiohttp-client
+		check_virtualenv
+	fi
 
 }
 
 function install_task_whois_service() {
-  cd /repo/py-aiohttp-client
-  cp deploy/task_whois.service /usr/lib/systemd/system/task_whois.service
-  sudo systemctl daemon-reload
-  sudo systemctl enable task_whois.service
-  sudo systemctl start task_whois.service
-  sudo journalctl -f -u task_whois.service
+	cd /repo/py-aiohttp-client
+	cp deploy/task_whois.service /usr/lib/systemd/system/task_whois.service
+	sudo systemctl daemon-reload
+	sudo systemctl enable task_whois.service
+	sudo systemctl start task_whois.service
+	sudo journalctl -f -u task_whois.service
 }
 
 function security_enhance() {
-  read -p "修改root密码[y/N]: " confirm
-  if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
-    red "开始修改root密码"
-    sudo passwd root
-  fi
-  read -p "禁止root通过ssh登陆[y/N]: " confirm
-  if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
-    sudo sed -i -E "s/PermitRootLogin prohibit-password/PermitRootLogin no/" /etc/ssh/sshd_config
-    sudo sed -i -E "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
-    sudo systemctl restart sshd.service
-  fi
-  read -p "增加fg用户[y/N]: " confirm
-  if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
-    sudo useradd -m -G sudo -s /bin/bash fg
-  fi
-  read -p "修改fg用户密码[y/N]: " confirm
-  if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
-    red "开始修改fg密码"
-    sudo passwd fg
-  fi
-  read -p "ssh改用55555端口[y/N]: " confirm
-  if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
-    sudo sed -i -E "s/#Port 22/Port 55555/" /etc/ssh/sshd_config
-    sudo systemctl restart sshd.service
-  fi
+	read -p "修改root密码[y/N]: " confirm
+	if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
+		red "开始修改root密码"
+		sudo passwd root
+	fi
+	read -p "禁止root通过ssh登陆[y/N]: " confirm
+	if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
+		sudo sed -i -E "s/PermitRootLogin prohibit-password/PermitRootLogin no/" /etc/ssh/sshd_config
+		sudo sed -i -E "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
+		sudo systemctl restart sshd.service
+	fi
+	read -p "增加fg用户[y/N]: " confirm
+	if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
+		sudo useradd -m -G sudo -s /bin/bash fg
+	fi
+	read -p "修改fg用户密码[y/N]: " confirm
+	if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
+		red "开始修改fg密码"
+		sudo passwd fg
+	fi
+	read -p "ssh改用55555端口[y/N]: " confirm
+	if [ "$confirm" = "Y" ] || [ "$confirm" = "y" ]; then
+		sudo sed -i -E "s/#Port 22/Port 55555/" /etc/ssh/sshd_config
+		sudo systemctl restart sshd.service
+	fi
+}
+
+function install_socks5_proxy() {
+	if [ ! -f /usr/bin/brook ]; then
+		echo "未安装brook socks5代理，开始安装"
+		sudo curl -L https://github.com/txthinking/brook/releases/latest/download/brook_linux_amd64 -o /usr/bin/brook
+		sudo chmod +x /usr/bin/brook
+	fi
+	read -p "输入socks5密码(默认741852): " confirm
+	if [ -z "$confirm" ]; then
+		confirm="741852"
+	fi
+	red "密码为：${confirm}"
+	# <<- 要求制表符不能为空格，必须为TAB
+	cat <<-EOF >/usr/lib/systemd/system/socks5.service
+		[Unit]
+		Description=Brook-socks5
+		After=network.target
+		Wants=network.target
+		
+		[Service]
+		WorkingDirectory=/repo
+		ExecStart=/usr/bin/brook server --listen :5556 --password ${confirm}
+		Restart=on-abnormal
+		RestartSec=5s
+		KillMode=mixed
+		
+		StandardOutput=null
+		StandardError=syslog
+		
+		[Install]
+		WantedBy=multi-user.target
+	EOF
+	sudo systemctl enable socks5.service
+	sudo systemctl start socks5.service
+	sudo journalctl -u socks5.service
+	red "安装socks5服务成功"
 }
 
 function start_menu() {
-  clear
-  red "============================"
-  red "                            "
-  red "    AIO Toolbox             "
-  echo "                           "
-  red "============================"
-  yellow "检测到VPS信息如下"
-  yellow "处理器架构：$arch"
-  yellow "虚拟化架构：$virt"
-  yellow "操作系统：$release"
-  yellow "内核版本：$kernelVer"
-  echo "                            "
-  green "下面是工具箱提供的一些功能:"
-  echo "1. 安全加固"
-  echo "2. 安装github命令行"
-  echo "3. 通过gh登陆github"
-  echo "4. 克隆或者更新客户端仓库"
-  echo "5. 安装task_whois服务"
-  echo "v. 更新脚本"
-  echo "0. 退出脚本"
-  read -p "请输入选项:" menuNumberInput
-  case "$menuNumberInput" in
-  "1")
-    security_enhance
-    ;;
-  "2")
-    install_github_cli
-    ;;
-  "3")
-    gh_login
-    ;;
-  "4")
-    clone_client_repo
-    ;;
-  "5")
-    install_task_whois_service
-    ;;
-  "v")
-    update_script
-    ;;
-  *)
-    red "退出"
-    ;;
-  esac
+	clear
+	red "============================"
+	red "                            "
+	red "    AIO Toolbox             "
+	echo "                           "
+	red "============================"
+	yellow "检测到VPS信息如下"
+	yellow "处理器架构：$arch"
+	yellow "虚拟化架构：$virt"
+	yellow "操作系统：$release"
+	yellow "内核版本：$kernelVer"
+	echo "                            "
+	green "下面是工具箱提供的一些功能:"
+	echo "1. 安全加固"
+	echo "2. 安装github命令行"
+	echo "3. 通过gh登陆github"
+	echo "4. 克隆或者更新客户端仓库"
+	echo "5. 安装task_whois服务"
+	echo "6. 安装Brook socks5服务"
+	echo "v. 更新脚本"
+	echo "0. 退出脚本"
+	read -p "请输入选项:" menuNumberInput
+	case "$menuNumberInput" in
+	"1")
+		security_enhance
+		;;
+	"2")
+		install_github_cli
+		;;
+	"3")
+		gh_login
+		;;
+	"4")
+		clone_client_repo
+		;;
+	"5")
+		install_task_whois_service
+		;;
+	"6")
+		install_socks5_proxy
+		;;
+	"v")
+		update_script
+		;;
+	*)
+		red "退出"
+		;;
+	esac
 }
 
 start_menu
