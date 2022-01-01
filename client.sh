d130002c3d03cf4d87585c9ab814dd2fba42b2e5
ethan <brilon@outlook.com>
2022-01-01 14:08:46 +0800

    update

--- client.sh
+++ client.sh
@@ -139,7 +139,8 @@ function check_virtualenv() {
     virtualenv venv
   fi
   source venv/bin/activate
-  python -m pip install pip
+  pip install --upgrade pip
+  pip install -r requirements.txt
 }
 
 function clone_client_repo() {
