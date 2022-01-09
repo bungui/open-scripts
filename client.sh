c4ad1fc6894f4187aff7eee5d7acd4a42ff163dd
ethan <brilon@outlook.com>
2022-01-09 23:43:01 +0800

    add tor exitnodes

--- client.sh
+++ client.sh
@@ -596,6 +596,11 @@ function install_tor() {
 		red "写入控制端口： $tor_port"
 		sudo echo "ControlPort $tor_port" >>/etc/tor/torrc
 	fi
+	if ! grep -q -i -E "^ExitNodes " /etc/tor/torrc; then
+		sudo echo "ExitNodes {us}" >> /etc/tor/torrc
+		sudo echo "StrictNodes 1" >> /etc/tor/torrc
+	fi
+
 	if ! grep -q -i -E "^HashedControlPassword " /etc/tor/torrc; then
 		red "写入控制密码： $tor_password"
 		sudo echo HashedControlPassword $(tor --hash-password "$tor_password" | tail -n 1) >>/etc/tor/torrc
