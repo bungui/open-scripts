ddd1e509bab26bb1f7a84c74eccfe0538488f1e3
ethan <brilon@outlook.com>
2022-01-15 14:04:59 +0800

    update

--- example/backup.sh
+++ example/backup.sh
@@ -9,6 +9,7 @@ date_str=$(date +%F)
 sudo mkdir -p "${backup_dir}"
 
 # mysql备份
+sudo find $backup_dir/mysql* -mindepth 1 -mtime +30 -delete -print
 sql_file="/tmp/mysql-${date_str}.sql"
 zip_file="/tmp/mysql-${date_str}.zip"
 sudo mysqldump --all-databases --single-transaction --quick --lock-tables=false -u root -p${mysql_pass} >"${sql_file}"
@@ -19,6 +20,7 @@ sudo rm "${zip_file}" -f
 echo "备份mysql成功"
 
 # redis备份
+sudo find $backup_dir/redis* -mindepth 1 -mtime +10 -delete -print
 redis_dump_file="/var/lib/redis/dump.rdb"
 redis_tmp_file="/tmp/dump.rdb"
 redis_zip_file="/tmp/redis-${date_str}.zip"
@@ -30,17 +32,10 @@ sudo rm "${redis_tmp_file}" -f
 echo "备份redis成功"
 
 # 备份webdav目录
+sudo find $backup_dir/hkwebdav* -mindepth 1 -mtime +5 -delete -print
 webdav_dir="/webdav"
-webdav_filename="hkwebdav_${date_str}.zip"
-webdav_tmp_file="/tmp/${webdav_filename}"
-webdav_file="${backup_dir}/${webdav_filename}"
-
-sudo zip -9 -r "${webdav_tmp_file}" "${webdav_dir}"
-sudo cp "$webdav_tmp_file" "$webdav_file"
-sudo rm "$webdav_tmp_file"
+webdav_file="${backup_dir}/hkwebdav_${date_str}.zip"
+sudo zip -9 -r "${webdav_file}" "${webdav_dir}"
 echo "备份本地webdav目录成功"
 
-# 需要等待一段时间，因为上传到webdav后，mtime才会是最新的
-sleep 600
-find "$backup_dir" -mindepth 1 -mtime +10 -delete -print
-echo "删除10天前的文件，并打印删除文件路径"
+# rclone同步到webdav前，文件的mtime是很旧的，不可以根据mtime删除文件
